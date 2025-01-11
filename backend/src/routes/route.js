35802a99a692e2fc6a35298455605055ffc201a4
SATHYAMOORTHY TS <sathyamoortthyts0105@gmail.com>
2025-01-11 06:12:25 +0530

    add some changes in stats routes

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -4,36 +4,53 @@ const router = express.Router();
 require('dotenv').config()
 const apikey = process.env.API_KEY;
 
+async function fetchRecentData(request) {
+     const url = `https://api.coingecko.com/api/v3/coins/markets`
+     console.log(request)
+     try {
+        const response = await axios.get(url ,{
+            headers : {
+                accept : "application/json",
+                Authorization : `Bearer ${apikey}`
+            },
+            params : { 
+
+                vs_currency : "usd",
+                ids : `${request}` 
+            }
+         } )
+         console.log(response.data)
+         return response.data
+     }
+     catch(err) {
+        //  console.error(err);
+         return "Error occured" + err.message
+     }
+}
+
 router.post('/stats',async (req,res)=>{
-    const request = req.params.coin;
+    const request = req.query.coin;
+    console.log(request
+    )
     if(request == ""){
         res.status(411).json({
-            msg : " Inputs required"
-
+            msg : "Invalid onputs or inputs needed"
         })
     }
-    try { 
 
-            const response = await axios.get(`https://api.coingecko.com/api/v3/coins/markets`, {
-                headers : {
-                    accept : "application/json",
-                    Authorization : `Bearer ${apikey}`
-                }, 
-                params : { 
-                    vs_currency : "usd",
-                    ids : request
-                }
-               }) 
-             res.status(200).json({
-                data : response.data
-             })
-            } catch(err) {
-                console.error(err)
-            res.status(403).json({
-                msg : "error happened : " + err
-            })
-           }   
+    try {
+        const DataResponse = await fetchRecentData(request);
+        res.status(200).json({
+            data : DataResponse
+        })
+    }catch(err){
+        res.status(403).json({
+            msg : "error happened : " + err.message
+        })
+    }
 })
+
+
 
 module.exports = {
     router
